82030568561b41fa740dabed9c63b4ba2899833c
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-11-11 01:07:02 +0100

    Feat: unborrow tests

---
+++ src/controllers/equipment/unborrow/unborrow.test.ts
@@ -0,0 +1,44 @@
+import ApiTest from '../../../../testing/helpers/test.helper';
+import { Response } from '../../../types/response.types';
+import { EquipmentModel } from '../../../models/equipment.model';
+import { Types } from 'mongoose';
+
+const COLLECTION = 'equipments';
+const equipment: Partial<EquipmentModel> = {
+    _id: new Types.ObjectId(),
+    name: 'Cable',
+    borrowedBy: ['Seb', 'Fred'],
+    amount: 2
+}
+
+const apiTest = new ApiTest('put', '/equipment');
+apiTest.beforeEach = () => apiTest.insertOne(COLLECTION, equipment);
+
+const equipmentId = equipment._id.toString();
+apiTest.create(() => {
+
+    apiTest.route(`/${equipmentId}/unborrow`, "Should successfully unborrow equipment.", async (request) => {
+        const borrower = 'Fred';
+        const response: Response<EquipmentModel> = await request().send({ borrower });
+        
+        const updatedEquipment = await apiTest.findById<EquipmentModel>(COLLECTION, equipment._id);
+
+        expect(response.statusCode).toBe(204);
+        expect(updatedEquipment?.borrowedBy).not.toContain(borrower);
+    });
+
+    apiTest.route(`/invalid/unborrow`, "Should throw an error for invalid id.", async (request) => {
+        const borrower = 'Fred';
+        const response: Response<EquipmentModel> = await request().send({ borrower });
+
+        expect(response.statusCode).toBe(404);
+    });
+
+    const randomId = new Types.ObjectId().toString();
+    apiTest.route(`/${randomId}/unborrow`, "Should throw an error when equipment not found.", async (request) => {
+        const borrower = 'Fred';
+        const response: Response<EquipmentModel> = await request().send({ borrower });
+
+        expect(response.statusCode).toBe(404);
+    });
+});
